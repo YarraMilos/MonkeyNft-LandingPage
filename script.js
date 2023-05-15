79eb36787d7e86ea8fef052c7662651710487de4
Emil <118768314+YarraMilos@users.noreply.github.com>
2023-05-16 00:29:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const hamb=document.querySelector(".burger__menu__icon"),popup=document.querySelector(".popup"),body=document.body;hamb.addEventListener("click",function(e){e.preventDefault(),popup.classList.toggle("open"),hamb.classList.toggle("active"),body.classList.toggle("noscroll")}),AOS.init({duration:1200});
+
+const hamb = document.querySelector(".burger__menu__icon");
+const popup = document.querySelector(".popup");
+const body = document.body;
+
+
+hamb.addEventListener("click", function(e) {
+  e.preventDefault();
+  popup.classList.toggle('open')
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+});
+
+
+
+AOS.init({
+  duration: 1200,
+})
